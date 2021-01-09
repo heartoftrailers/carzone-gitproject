06dbcc8790ba838fb1a55416f7afc4cde106ccf1
Florin Alexandru Ciovica <74630109+heartoftrailers@users.noreply.github.com>
2021-01-09 23:55:25 +0000

    Update settings.py

--- carzone/settings.py
+++ carzone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qpucp#!l8%+7i+bj9v$+km+18ua*fqt02d^*_2---oy2e&r5#x'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

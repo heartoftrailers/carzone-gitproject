c2eb5c0c1f2157c73cc80b804bf941141a7fcb50
heartoftrailers <ciovicaflorin0@gmail.com>
2021-01-11 02:57:08 +0000

    created pages + conf navigation links

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path
-from . import views
+from pages import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('servies', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
 ]
